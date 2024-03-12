e7b561101c9537f3235f9db69554a4fc92903bff
guille alonso <guilloalonsot@gmail.com>
2024-03-12 16:33:56 -0300

    limpiando template

--- components/shop/common-layout.js
+++ components/shop/common-layout.js
@@ -15,13 +15,13 @@ const CommonLayout = ({ children, title, parent, subTitle }) => {
       <HeaderOne topClass="top-header" logoName="logo.png" />
       <Breadcrubs title={title} parent={parent} subTitle={subTitle} />
       <>{children}</>
-      <MasterFooter
+      {/* <MasterFooter
         footerClass={`footer-light `}
         footerLayOut={"light-layout upper-footer"}
         footerSection={"small-section border-section border-top-0"}
         belowSection={"section-b-space light-layout"}
         newLatter={true}
-      />
+      /> */}
     </>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -22,21 +22,21 @@ const Fashion = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/x-icon" href={"/assets/images/favicon/1.png"} />
       </Helmet>
-      <ModalComponent />
+      {/* <ModalComponent /> */}
       <HeaderOne logoName={"logo.png"} topClass="top-header" />
-      <Banner />
-      <CollectionBanner />
+      {/* <Banner /> */}
+      {/* <CollectionBanner /> */}
       <Paragraph title="title1 section-t-space" inner="title-inner1" hrClass={false} />
       <TopCollection noTitle="null" backImage={true} type="fashion" title="top collection" subtitle="special offer" productSlider={Product4} designClass="section-b-space p-t-0 ratio_asos px-2" noSlider="false" cartClass="cart-info cart-wrap" />
       <Parallax />
       <SpecialProducts type="fashion" backImage={true} productSlider={Product4} line={true} title="title1 section-t-space" inner="title-inner1" designClass="section-b-space p-t-0 ratio_asos" noSlider="true" cartClass="cart-info cart-wrap" />
-      <ServiceLayout sectionClass="border-section small-section" />
-      <Blog type="fashion" title="title1" inner="title-inner1" />
-      <Instagram type="fashion" />
-      <div className="section-b-space">
+      {/* <ServiceLayout sectionClass="border-section small-section" /> */}
+      {/* <Blog type="fashion" title="title1" inner="title-inner1" /> */}
+      {/* <Instagram type="fashion" /> */}
+      {/* <div className="section-b-space">
         <LogoBlock />
-      </div>
-      <MasterFooter footerClass={`footer-light`} footerLayOut={"light-layout upper-footer"} footerSection={"small-section border-section border-top-0"} belowSection={"section-b-space light-layout"} newLatter={true} logoName={"logo.png"} />
+      </div> */}
+      {/* <MasterFooter footerClass={`footer-light`} footerLayOut={"light-layout upper-footer"} footerSection={"small-section border-section border-top-0"} belowSection={"section-b-space light-layout"} newLatter={true} logoName={"logo.png"} /> */}
     </>
   );
 };
